3e7442775eb08e762eaa0adfcaabb1188b37a2d7
Yash Patel <yp120@ic.ac.uk>
2020-11-13 10:54:18 +0000

    Finished all functions and finished I/O functions

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -64,9 +64,27 @@ diags (cs, n)
 
 -------------------------------------------------------------------
 
+--This functions checks if the game has a winner i.e. there is a
+--column or a row that contains either all X's or all O's
 gameOver :: Board -> Bool
-gameOver
-  = undefined
+gameOver b
+  = foldl1 (||) (cols' ++ rows' ++ diag')
+  where
+    cols' = checkList (map nub (cols b))
+    rows' = checkList (map nub (rows b))
+    diag' = checkList (map nub (diags b))
+    checkList (x : xs)
+      | elemEmpty  = False : checkList xs
+      | takenXOnly = [True]
+      | takenOonly = [True]
+      | otherwise  = False : checkList xs
+      where
+        elemTakenX = (Taken X `elem` x)
+        elemTakenO = (Taken O `elem` x)
+        elemEmpty  = (Empty `elem` x)
+        takenXOnly = elemTakenX && not(elemTakenO)
+        takenOonly = not(elemTakenX) && elemTakenO 
+    checkList [] = [False]
 
 -------------------------------------------------------------------
 
@@ -75,19 +93,54 @@ gameOver
 -- separated by whitespace. Bounds checking happens in tryMove, not here.
 --
 parsePosition :: String -> Maybe Position
-parsePosition
-  = undefined
+parsePosition "" = Nothing
+parsePosition s
+  | null x3   = readMaybe ("(" ++ x1 ++ "," ++ x2 ++ ")")
+  | otherwise = Nothing   
+  where
+    x1 = head (words s)
+    (x2 : x3) = tail (words s)
 
+--tryMove will take a player, positon and baord
+--returns Maybe Board
+--function checks if it can place a player at the position
+--if not, returns Nothing, if yes, returns Just Board
 tryMove :: Player -> Position -> Board -> Maybe Board
-tryMove
-  = undefined
+tryMove p (x, y) board@(t, n)
+  | checkEmpty t 0 = Just ((replace pos (Taken p) t), n)
+  | otherwise      = Nothing
+  where
+    pos = (x * n) + y 
+    checkEmpty [] n = False
+    checkEmpty (t : ts) n
+      | (n == pos) && (t == Empty) = True
+      | (n == pos) && (t /= Empty) = False
+      | otherwise                  = checkEmpty ts (n+1)
 
 -------------------------------------------------------------------
 -- I/O Functions
 
+--PrettyPrint is done by using the given functions
+--to seperate the rows to print out
+--each row is then mapped to a string depending on its type
+--each row is printed one by one through recursion
 prettyPrint :: Board -> IO ()
-prettyPrint
-  = undefined
+prettyPrint b@(c : cs, n)
+  = prettyPrint' rs
+  where
+    rs = map unwords (map (map (c')) (rows b))
+    c' c
+      | c == Taken O = "O"
+      | c == Taken X = "X"
+      | otherwise    = "-"
+    prettyPrint' [] = do putStr ""
+    prettyPrint' (r : r')
+      = do
+          putStrLn r
+          prettyPrint' r'
+
+prettyPrint ([] , _) = do putStrLn ""
+
 
 -- The following reflect the suggested structure, but you can manage the game
 -- in any way you see fit.
@@ -95,22 +148,64 @@ prettyPrint
 -- Repeatedly read a target board position and invoke tryMove until
 -- the move is successful (Just ...).
 takeTurn :: Board -> Player -> IO Board
-takeTurn
-  = undefined
+takeTurn b p
+  = do
+    putStrLn ""
+    putStr $ "Player " ++ show p ++ ", make your move (row col) " 
+    pos <- getLine
+    case parsePosition pos of
+      Nothing -> putStrLn invInp >> recur
+      Just pos ->
+        case tryMove p pos b of
+          Nothing -> putStrLn invPos >> recur
+          Just pos -> return pos
+    where
+      recur = takeTurn b p
+      invInp = "Invalid input, try again"
+      invPos = "Position taken/invalid, try again"
 
 -- Manage a game by repeatedly: 1. printing the current board, 2. using
 -- takeTurn to return a modified board, 3. checking if the game is over,
 -- printing the board and a suitable congratulatory message to the winner
 -- if so.
-playGame :: Board -> Player -> IO ()
-playGame
-  = undefined
+playGame :: Player -> Board -> IO ()
+playGame p b
+  = do
+    prettyPrint b
+    case gameOver b of
+      True -> putStrLn $ "Congratulations, " ++ show p ++ ", has won the game!"
+      False ->
+        case p of
+          O ->
+            do
+            takeTurn b X >>= playGame X
+          X ->
+            do
+            takeTurn b O >>= playGame O
+    where
+      -- xplay = takeTurn b X
+      -- oplay = takeTurn b O
+      winMsg p' = putStrLn ("Congratulations, " ++ (show p') ++ ", you have won the game!")
+
 
 -- Print a welcome message, read the board dimension, invoke playGame and
 -- exit with a suitable message.
 main :: IO ()
 main
-  = undefined
+  = do
+    putStrLn "Welcome to tic-tac-toe!"
+    putStr "Enter board size: "
+    bSize <- getLine
+    do
+    case readMaybe bSize :: Maybe Int of
+      Nothing -> putStrLn invInp >> main
+      Just bSize ->
+        playGame O (createBoard bSize)   
+    where
+      invInp = "Invalid input, try again"
+      createBoard :: Int -> Board
+      createBoard n = (take (n*n) (repeat Empty), n) :: Board
+
 
 -------------------------------------------------------------------
 
